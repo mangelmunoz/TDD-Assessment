da3e2bbf25c1165c2d9b21627a3de06608614444
Jorge Ruiz <jorge@macdiego.com>
2022-10-31 12:44:36 +0100

    first commit

---
+++ src/main/java/com/example/TDDAssessmentBackend/TddAssessmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TDDAssessmentBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAssessmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddAssessmentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TDDAssessmentBackend/TddAssessmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TDDAssessmentBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddAssessmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
