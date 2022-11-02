12c87dbc0a63731880ecde40c69f6cff9f98a26f
Jorge Ruiz <jorge@macdiego.com>
2022-11-02 10:55:11 +0100

    Flights with return & Flights with scale

--- PersistenceLayer/src/main/java/com/example/TDDAssessmentBackend/TddAssessmentBackendApplication.java
+++ PersistenceLayer/src/main/java/com/example/TDDAssessmentBackend/TddAssessmentBackendApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class TddAssessmentBackendApplication {
@@ -26,9 +27,9 @@ public class TddAssessmentBackendApplication {
 			Flight flight1 = persistenceService.addFlight(new Flight(1, "IBE-1234","IBERIA", LocalDateTime.now().plusDays(2), LocalDateTime.now().plusDays(2).plusHours(2), new Country(1, ECountry.VALENCIA), new Country(2, ECountry.CORDOBA), 200.0, 50, true, false, null, new ArrayList<>(), new ArrayList<>()));
 			Flight flight2 = persistenceService.addFlight(new Flight(2, "IBE-2234","IBERIA", LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(1).plusHours(2), new Country(2, ECountry.CORDOBA), new Country(1, ECountry.VALENCIA), 200.0, 50, true, true, flight1, new ArrayList<>(), new ArrayList<>()));
 
-//			Flight flight3 = persistenceService.addFlight(new Flight(3, "RYA-2234","RYANAIR", LocalDateTime.now().plusDays(6), LocalDateTime.now().plusDays(6).plusHours(2), new Country(2, ECountry.CORDOBA), new Country(1, ECountry.VALENCIA), 200.0, 50, true, true, flight1, new ArrayList<>(), new ArrayList<>()));
-//			Flight flight4 = persistenceService.addFlight(new Flight(4, "RYA-2234","RYANAIR", LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(5).plusHours(9), new Country(2, ECountry.CORDOBA), new Country(1, ECountry.VALENCIA), 200.0, 50, true, true, flight1, new ArrayList<>(), new ArrayList<>()));
-//			Flight flight5 = persistenceService.addFlight(new Flight(5, "RYA-2234","RYANAIR", LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(5).plusHours(3), new Country(2, ECountry.CORDOBA), new Country(3, ECountry.TORREDONJIMENO), 200.0, 50, true, true, flight1, new ArrayList<>(), new ArrayList<>()));
+			Flight flight3 = persistenceService.addFlight(new Flight(3, "RYA-2234","RYANAIR", LocalDateTime.now().plusDays(6), LocalDateTime.now().plusDays(6).plusHours(2), new Country(4, ECountry.TARRAGONA), new Country(5, ECountry.SPARTA), 200.0, 50, true, false, null, new ArrayList<>(), new ArrayList<>()));
+			Flight flight4 = persistenceService.addFlight(new Flight(4, "RYA-2234","RYANAIR", LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(5).plusHours(9), new Country(3, ECountry.TORREDONJIMENO), new Country(4, ECountry.TARRAGONA), 80.0, 50, true, false, null, new ArrayList<>(), new ArrayList<>()));
+			Flight flight5 = persistenceService.addFlight(new Flight(5, "RYA-2234","RYANAIR", LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(5).plusHours(3), new Country(3, ECountry.TORREDONJIMENO), new Country(5, ECountry.SPARTA), 250.0, 50, true, false, null, List.of(new Country(4, ECountry.TARRAGONA)), new ArrayList<>()));
 
 
 		};
